ef463c0a39369d9704995bbc08204e2c3dae7acc
Mishutkaaa <mishnevsky@aboba.fun>
2024-12-03 21:18:36 +0500

    second commit

--- main.go
+++ main.go
@@ -8,69 +8,6 @@ import (
 	"sync"
 )
 
-type Product struct {
-	ID          int    `json:"id"`
-	Name        string `json:"name"`
-	Article     string `json:"article"`
-	Code        string `json:"code"`
-	Description string `json:"description"`
-	Composition string `json:"composition"`
-	Care        string `json:"care"`
-	Models      []Model `json:"models"`
-}
-
-type Model struct {
-	ID     int    `json:"id"`
-	Code   string `json:"code"`
-	Color  Color  `json:"color"`
-	Photo  Photo  `json:"photo"`
-	Skus   []Sku  `json:"skus"`
-	Medias []Media `json:"medias"`
-}
-
-type Color struct {
-	ID   int    `json:"id"`
-	Hex  string `json:"hex"`
-	Name string `json:"name"`
-}
-
-type Photo struct {
-	ID     int    `json:"id"`
-	Slot   int    `json:"slot"`
-	Width  int    `json:"width"`
-	Height int    `json:"height"`
-	Type   int    `json:"type"`
-	URL    string `json:"url"`
-}
-
-type Sku struct {
-	ID    int   `json:"id"`
-	Size  Size  `json:"size"`
-	Stock Stock `json:"stock"`
-	Price int   `json:"price"`
-}
-
-type Size struct {
-	ID    int    `json:"id"`
-	Unit  string `json:"unit"`
-	Age   string `json:"age"`
-	Value string `json:"value"`
-}
-
-type Stock struct {
-	Online  int `json:"online"`
-	Offline int `json:"offline"`
-}
-
-type Media struct {
-	ID     int    `json:"id"`
-	Slot   int    `json:"slot"`
-	Width  int    `json:"width"`
-	Height int    `json:"height"`
-	Type   int    `json:"type"`
-	URL    string `json:"url"`
-}
-
 type ProductData struct {
 	Name        string                 `json:"name"`
 	Price       int                    `json:"price"`
@@ -79,7 +16,7 @@ type ProductData struct {
 	Description string                 `json:"description"`
 	Color       []map[string]interface{} `json:"color"`
 	Article     string                 `json:"article"`
-	ProductURL  string                 `json:"product_url"`
+	ProductURL  []string               `json:"product_url"`
 	Category    string                 `json:"category"`
 	ProductID   int                    `json:"product_id"`
 	Composition string                 `json:"composition"`
@@ -91,37 +28,37 @@ func main() {
 	urls := []string{
 		"https://lime-shop.com/api/section/outerwear?page=%d&page_size=30",
 		"https://lime-shop.com/api/section/knitwear?page=%d&page_size=30",
-		"https://lime-shop.com/api/section/blazers?page=%d&page_size=30",
-		"https://lime-shop.com/api/section/waistcoats?page=%d&page_size=30",
-		"https://lime-shop.com/api/section/trousers?page=%d&page_size=30",
-		"https://lime-shop.com/api/section/suits?page=%d&page_size=30",
-		"https://lime-shop.com/api/section/dresses?page=%d&page_size=30",
-		"https://lime-shop.com/api/section/women_shirts_all?page=%d&page_size=30",
-		"https://lime-shop.com/api/section/skirts?page=%d&page_size=30",
-		"https://lime-shop.com/api/section/co_ord_sets?page=%d&page_size=30",
-		"https://lime-shop.com/api/section/down_jackets?page=%d&page_size=30",
-		"https://lime-shop.com/api/section/women_sweaters_cardigans?page=%d&page_size=30",
-		"https://lime-shop.com/api/section/t_shirts?page=%d&page_size=30",
-		"https://lime-shop.com/api/section/women_jeans?page=%d&page_size=30",
-		"https://lime-shop.com/api/section/sweatshirts?page=%d&page_size=30",
-		"https://lime-shop.com/api/section/tops?page=%d&page_size=30",
-		"https://lime-shop.com/api/section/shorty?page=%d&page_size=30",
-		"https://lime-shop.com/api/section/sportswear?page=%d&page_size=30",
-		"https://lime-shop.com/api/section/all_shoes?page=%d&page_size=30",
-		"https://lime-shop.com/api/section/bags?page=%d&page_size=30",
-		"https://lime-shop.com/api/section/accessories?page=%d&page_size=30",
-		"https://lime-shop.com/api/section/jewellery?page=%d&page_size=30",
-		"https://lime-shop.com/api/section/underwear?page=%d&page_size=30",
-		"https://lime-shop.com/api/section/loungewear?page=%d&page_size=30",
-		"https://lime-shop.com/api/section/women_winter_sets?page=%d&page_size=30",
-		"https://lime-shop.com/api/section/women_wool?page=%d&page_size=30",
-		"https://lime-shop.com/api/section/women_basic_wardrobe?page=%d&page_size=30",
-		"https://lime-shop.com/api/section/last_sizes?page=%d&page_size=30",
-		"https://lime-shop.com/api/section/limited_edition?page=%d&page_size=30",
-		"https://lime-shop.com/api/section/kids_girls_view_all?page=%d&page_size=30",
-		"https://lime-shop.com/api/section/kids_boys_view_all?page=%d&page_size=30",
-		"https://lime-shop.com/api/section/kids_baby_girls_view_all?page=%d&page_size=30",
-		"https://lime-shop.com/api/section/kids_baby_boys_view_all?page=%d&page_size=30",
+    "https://lime-shop.com/api/section/blazers?page=%d&page_size=30",
+    "https://lime-shop.com/api/section/waistcoats?page=%d&page_size=30",
+    "https://lime-shop.com/api/section/trousers?page=%d&page_size=30",
+    "https://lime-shop.com/api/section/suits?page=%d&page_size=30",
+    "https://lime-shop.com/api/section/dresses?page=%d&page_size=30",
+    "https://lime-shop.com/api/section/women_shirts_all?page=%d&page_size=30",
+    "https://lime-shop.com/api/section/skirts?page=%d&page_size=30",
+    "https://lime-shop.com/api/section/co_ord_sets?page=%d&page_size=30",
+    "https://lime-shop.com/api/section/down_jackets?page=%d&page_size=30",
+    "https://lime-shop.com/api/section/women_sweaters_cardigans?page=%d&page_size=30",
+    "https://lime-shop.com/api/section/t_shirts?page=%d&page_size=30",
+    "https://lime-shop.com/api/section/women_jeans?page=%d&page_size=30",
+    "https://lime-shop.com/api/section/sweatshirts?page=%d&page_size=30",
+    "https://lime-shop.com/api/section/tops?page=%d&page_size=30",
+    "https://lime-shop.com/api/section/shorty?page=%d&page_size=30",
+    "https://lime-shop.com/api/section/sportswear?page=%d&page_size=30",
+    "https://lime-shop.com/api/section/all_shoes?page=%d&page_size=30",
+    "https://lime-shop.com/api/section/bags?page=%d&page_size=30",
+    "https://lime-shop.com/api/section/accessories?page=%d&page_size=30",
+    "https://lime-shop.com/api/section/jewellery?page=%d&page_size=30",
+    "https://lime-shop.com/api/section/underwear?page=%d&page_size=30",
+    "https://lime-shop.com/api/section/loungewear?page=%d&page_size=30",
+    "https://lime-shop.com/api/section/women_winter_sets?page=%d&page_size=30",
+    "https://lime-shop.com/api/section/women_wool?page=%d&page_size=30",
+    "https://lime-shop.com/api/section/women_basic_wardrobe?page=%d&page_size=30",
+    "https://lime-shop.com/api/section/last_sizes?page=%d&page_size=30",
+    "https://lime-shop.com/api/section/limited_edition?page=%d&page_size=30",
+    "https://lime-shop.com/api/section/kids_girls_view_all?page=%d&page_size=30",
+    "https://lime-shop.com/api/section/kids_boys_view_all?page=%d&page_size=30",
+    "https://lime-shop.com/api/section/kids_baby_girls_view_all?page=%d&page_size=30",
+    "https://lime-shop.com/api/section/kids_baby_boys_view_all?page=%d&page_size=30",
 	}
 
 	var wg sync.WaitGroup
@@ -221,104 +158,141 @@ func parseURL(url string, productChan chan<- ProductData) {
 				continue
 			}
 
-			model := models[0].(map[string]interface{})
-			skus, ok := model["skus"].([]interface{})
-			if !ok || len(skus) == 0 {
-				fmt.Printf("Key 'skus' not found or empty for model in URL %s\n", url)
-				continue
+			for _, model := range models {
+				modelMap, ok := model.(map[string]interface{})
+				if !ok {
+					fmt.Printf("Model is not a map for product in URL %s\n", url)
+					continue
+				}
+
+				skus, ok := modelMap["skus"].([]interface{})
+				if !ok || len(skus) == 0 {
+					fmt.Printf("Key 'skus' not found or empty for model in URL %s\n", url)
+					continue
+				}
+
+				sku := skus[0].(map[string]interface{})
+
+				// Проверка наличия ключа "medias"
+				medias, ok := modelMap["medias"].([]interface{})
+				if !ok {
+					fmt.Printf("Key 'medias' not found or not a slice for model in URL %s\n", url)
+					continue
+				}
+
+				var photoURLs []string
+				for _, media := range medias {
+					mediaMap, ok := media.(map[string]interface{})
+					if !ok {
+						fmt.Printf("Media is not a map for model in URL %s\n", url)
+						continue
+					}
+
+					mediaURL, ok := mediaMap["url"].(string)
+					if !ok {
+						fmt.Printf("Key 'url' not found or not a string in 'media' for model in URL %s\n", url)
+						continue
+					}
+
+					photoURLs = append(photoURLs, mediaURL)
+				}
+
+				// Проверка наличия ключей "name", "price", "old_price", "description", "article", "category", "id", "composition", "care"
+				name, ok := product["name"].(string)
+				if !ok {
+					fmt.Printf("Key 'name' not found or not a string for product in URL %s\n", url)
+					continue
+				}
+
+				price, ok := sku["price"].(float64)
+				if !ok {
+					fmt.Printf("Key 'price' not found or not a float64 for SKU in URL %s\n", url)
+					continue
+				}
+
+				// Проверка наличия ключа "old_price"
+				var oldPrice float64
+				if oldPriceVal, ok := sku["old_price"].(float64); ok {
+					oldPrice = oldPriceVal
+				} else {
+					oldPrice = 0 // Устанавливаем значение по умолчанию, если ключ отсутствует
+				}
+
+				description, ok := product["description"].(string)
+				if !ok {
+					fmt.Printf("Key 'description' not found or not a string for product in URL %s\n", url)
+					continue
+				}
+
+				article, ok := product["article"].(string)
+				if !ok {
+					fmt.Printf("Key 'article' not found or not a string for product in URL %s\n", url)
+					continue
+				}
+
+				category, ok := modelMap["category"].(string)
+				if !ok {
+					fmt.Printf("Key 'category' not found or not a string for model in URL %s\n", url)
+					continue
+				}
+
+				productID, ok := product["id"].(float64)
+				if !ok {
+					fmt.Printf("Key 'id' not found or not a float64 for product in URL %s\n", url)
+					continue
+				}
+
+				composition, ok := product["composition"].(string)
+				if !ok {
+					fmt.Printf("Key 'composition' not found or not a string for product in URL %s\n", url)
+					continue
+				}
+
+				care, ok := product["care"].(string)
+				if !ok {
+					fmt.Printf("Key 'care' not found or not a string for product in URL %s\n", url)
+					continue
+				}
+
+				// Извлечение полей "product.code" и "models.code"
+				productCode, ok := product["code"].(string)
+				
+				if !ok {
+					fmt.Printf("Key 'code' not found or not a string for product in URL %s\n", url)
+					continue
+				}
+
+				modelCode, ok := modelMap["code"].(string)
+				
+				if !ok {
+					fmt.Printf("Key 'code' not found or not a string for model in URL %s\n", url)
+					continue
+				}
+
+
+
+				// Генерация ссылки на продукт
+				productURL := generateProductURL(productCode, modelCode)
+
+				// Извлечение данных
+				productData := ProductData{
+					Name:        name,
+					Price:       int(price),
+					OldPrice:    int(oldPrice),
+					Size:        getSizes(skus),
+					Description: description,
+					Color:       getColors(models),
+					Article:     article,
+					ProductURL:  []string{productURL}, // Добавляем сгенерированную ссылку
+					Category:    category,
+					ProductID:   int(productID),
+					Composition: composition,
+					Care:        care,
+				}
+
+				// Отправка данных в канал
+				productChan <- productData
 			}
-
-			sku := skus[0].(map[string]interface{})
-
-			// Проверка наличия ключа "photo"
-			photo, ok := model["photo"].(map[string]interface{})
-			if !ok {
-				fmt.Printf("Key 'photo' not found or not a map for model in URL %s\n", url)
-				continue
-			}
-
-			// Проверка наличия ключа "url" в "photo"
-			photoURL, ok := photo["url"].(string)
-			if !ok {
-				fmt.Printf("Key 'url' not found or not a string in 'photo' for model in URL %s\n", url)
-				continue
-			}
-
-			// Проверка наличия ключей "name", "price", "old_price", "description", "article", "category", "id", "composition", "care"
-			name, ok := product["name"].(string)
-			if !ok {
-				fmt.Printf("Key 'name' not found or not a string for product in URL %s\n", url)
-				continue
-			}
-
-			price, ok := sku["price"].(float64)
-			if !ok {
-				fmt.Printf("Key 'price' not found or not a float64 for SKU in URL %s\n", url)
-				continue
-			}
-
-			// Проверка наличия ключа "old_price"
-			var oldPrice float64
-			if oldPriceVal, ok := sku["old_price"].(float64); ok {
-				oldPrice = oldPriceVal
-			} else {
-				oldPrice = 0 // Устанавливаем значение по умолчанию, если ключ отсутствует
-			}
-
-			description, ok := product["description"].(string)
-			if !ok {
-				fmt.Printf("Key 'description' not found or not a string for product in URL %s\n", url)
-				continue
-			}
-
-			article, ok := product["article"].(string)
-			if !ok {
-				fmt.Printf("Key 'article' not found or not a string for product in URL %s\n", url)
-				continue
-			}
-
-			category, ok := model["category"].(string)
-			if !ok {
-				fmt.Printf("Key 'category' not found or not a string for model in URL %s\n", url)
-				continue
-			}
-
-			productID, ok := product["id"].(float64)
-			if !ok {
-				fmt.Printf("Key 'id' not found or not a float64 for product in URL %s\n", url)
-				continue
-			}
-
-			composition, ok := product["composition"].(string)
-			if !ok {
-				fmt.Printf("Key 'composition' not found or not a string for product in URL %s\n", url)
-				continue
-			}
-
-			care, ok := product["care"].(string)
-			if !ok {
-				fmt.Printf("Key 'care' not found or not a string for product in URL %s\n", url)
-				continue
-			}
-
-			// Извлечение данных
-			productData := ProductData{
-				Name:        name,
-				Price:       int(price),
-				OldPrice:    int(oldPrice),
-				Size:        getSizes(skus),
-				Description: description,
-				Color:       getColors(models),
-				Article:     article,
-				ProductURL:  photoURL,
-				Category:    category,
-				ProductID:   int(productID),
-				Composition: composition,
-				Care:        care,
-			}
-
-			// Отправка данных в канал
-			productChan <- productData
 		}
 	}
 }
@@ -345,4 +319,8 @@ func getColors(models []interface{}) []map[string]interface{} {
 		})
 	}
 	return colors
+}
+
+func generateProductURL(productCode string, modelCode string) string {
+	return fmt.Sprintf("https://lime-shop.com/api/v2/product/%s?id=%s&force=false&model=false&color=%s", productCode, productCode, modelCode)
 }
