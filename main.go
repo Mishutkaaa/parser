d87992e95c7c9bcb3be1b800011e753e25cb403c
Mishutkaaa <mishnevsky@aboba.fun>
2024-12-04 20:11:39 +0500

    fourth commit

--- main.go
+++ main.go
@@ -5,118 +5,108 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strings"
 	"sync"
 )
 
 type ProductData struct {
-	Name        string        `json:"name"`
-	Price       int           `json:"price"`
-	OldPrice    int           `json:"old_price"`
-	Size        []Size        `json:"size"`
-	Count 		[]Stock `json:"stock"`
-	Description string        `json:"description"`
-	Color       []Color       `json:"color"`
-	Article     string        `json:"article"`
-	ProductURL  []string      `json:"product_url"`
-	Category    string        `json:"category"`
-	ProductID   int           `json:"product_id"`
-	Composition string        `json:"composition"`
-	Care        string        `json:"care"`
-	Medias      []string      `json:"medias"`
+	Name        string   `json:"name"`
+	Price       int      `json:"price"`
+	OldPrice    int      `json:"old_price"`
+	Size        []Size   `json:"size"`
+	Count       []Stock  `json:"stock"`
+	Description string   `json:"description"`
+	Color       []Color  `json:"color"`
+	Article     string   `json:"article"`
+	ProductURL  []string `json:"product_url"`
+	Category    string   `json:"category"`
+	ProductID   int      `json:"product_id"`
+	Composition string   `json:"composition"`
+	Care        string   `json:"care"`
+	Medias      []string `json:"medias"`
 }
 
 type Size struct {
 	Value string `json:"value"`
-
 }
-type Stock struct{
-		Online int `json:"online"`
+
+type Stock struct {
+	Online  int `json:"online"`
 	Offline int `json:"offline"`
 }
-type Color struct {
 
-	Name     string `json:"name"`
+type Color struct {
+	Name string `json:"name"`
 }
 
-type Product struct {
-	Name        string   `json:"name"`
-	Description string   `json:"description"`
-	Code string `json:"code"`
-	Article     string   `json:"article"`
-	Composition string   `json:"composition"`
-	Care        string   `json:"care"`
-	ProductID   int      `json:"id"` 
-	Models      []Model  `json:"models"`
+type Item struct {
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
+	Code        string  `json:"code"`
+	Article     string  `json:"article"`
+	Composition string  `json:"composition"`
+	Care        string  `json:"care"`
+	ProductID   int     `json:"id"`
+	Models      []Model `json:"models"`
 }
 
 type Model struct {
-	Code string			`json:"code"`
-	Category string   `json:"category"`
-	Medias   []Media  `json:"medias"`
-	Skus     []Sku    `json:"skus"`
-	Color    Color    `json:"color"`
+	Code     string  `json:"code"`
+	Category string  `json:"category"`
+	Medias   []Media `json:"medias"`
+	Skus     []Sku   `json:"skus"`
+	Color    Color   `json:"color"`
 }
 
 type Media struct {
-	URLs string `json:"url"`
+	ID     int    `json:"id"`
+	Slot   int    `json:"slot"`
+	Width  int    `json:"width"`
+	Height int    `json:"height"`
+	Type   int    `json:"type"`
+	URL    string `json:"url"`
 }
 
 type Sku struct {
-	Price    int    `json:"price"`
-	OldPrice int    `json:"old_price"`
-	Size     Size   `json:"size"`
-	Stock Stock `json:"stock"`
+	Price    int   `json:"price"`
+	OldPrice int   `json:"old_price"`
+	Size     Size  `json:"size"`
+	Stock    Stock `json:"stock"`
 }
 
+type Category struct {
+	URL    string     `json:"url"`
+	Items  []Category `json:"items"`
+}
+
+
+
 func main() {
-	urls := []string{
-"https://lime-shop.com/api/section/outerwear?page=%d&page_size=30",
-		"https://lime-shop.com/api/section/knitwear?page=%d&page_size=30",
-    "https://lime-shop.com/api/section/blazers?page=%d&page_size=30",
-    "https://lime-shop.com/api/section/waistcoats?page=%d&page_size=30",
-    "https://lime-shop.com/api/section/trousers?page=%d&page_size=30",
-    "https://lime-shop.com/api/section/suits?page=%d&page_size=30",
-    "https://lime-shop.com/api/section/dresses?page=%d&page_size=30",
-    "https://lime-shop.com/api/section/women_shirts_all?page=%d&page_size=30",
-    "https://lime-shop.com/api/section/skirts?page=%d&page_size=30",
-    "https://lime-shop.com/api/section/co_ord_sets?page=%d&page_size=30",
-    "https://lime-shop.com/api/section/down_jackets?page=%d&page_size=30",
-    "https://lime-shop.com/api/section/women_sweaters_cardigans?page=%d&page_size=30",
-    "https://lime-shop.com/api/section/t_shirts?page=%d&page_size=30",
-    "https://lime-shop.com/api/section/women_jeans?page=%d&page_size=30",
-    "https://lime-shop.com/api/section/sweatshirts?page=%d&page_size=30",
-    "https://lime-shop.com/api/section/tops?page=%d&page_size=30",
-    "https://lime-shop.com/api/section/shorty?page=%d&page_size=30",
-    "https://lime-shop.com/api/section/sportswear?page=%d&page_size=30",
-    "https://lime-shop.com/api/section/all_shoes?page=%d&page_size=30",
-    "https://lime-shop.com/api/section/bags?page=%d&page_size=30",
-    "https://lime-shop.com/api/section/accessories?page=%d&page_size=30",
-    "https://lime-shop.com/api/section/jewellery?page=%d&page_size=30",
-    "https://lime-shop.com/api/section/underwear?page=%d&page_size=30",
-    "https://lime-shop.com/api/section/loungewear?page=%d&page_size=30",
-    "https://lime-shop.com/api/section/women_winter_sets?page=%d&page_size=30",
-    "https://lime-shop.com/api/section/women_wool?page=%d&page_size=30",
-    "https://lime-shop.com/api/section/women_basic_wardrobe?page=%d&page_size=30",
-    "https://lime-shop.com/api/section/last_sizes?page=%d&page_size=30",
-    "https://lime-shop.com/api/section/limited_edition?page=%d&page_size=30",
-    "https://lime-shop.com/api/section/kids_girls_view_all?page=%d&page_size=30",
-    "https://lime-shop.com/api/section/kids_boys_view_all?page=%d&page_size=30",
-    "https://lime-shop.com/api/section/kids_baby_girls_view_all?page=%d&page_size=30",
-    "https://lime-shop.com/api/section/kids_baby_boys_view_all?page=%d&page_size=30",
+	menuURLs := []string{
+		"https://lime-shop.com/api/menu/left_women",
+		"https://lime-shop.com/api/menu/left_men",
+		"https://lime-shop.com/api/menu/left_kids",
 	}
 
 	var wg sync.WaitGroup
 	productChan := make(chan ProductData, 100)
 
+	for _, menuURL := range menuURLs {
+		urls, err := getCategoryURLs(menuURL)
+		if err != nil {
+			fmt.Println("Error getting category URLs:", err)
+			continue
+		}
 
-	for _, urlTemplate := range urls {
-		for page := 1; page <= 7; page++ {
-			wg.Add(1)
-			go func(urlTemplate string, page int) {
-				defer wg.Done()
-				url := fmt.Sprintf(urlTemplate, page)
-				parseURL(url, productChan)
-			}(urlTemplate, page)
+		for _, urlTemplate := range urls {
+			for page := 1; page <= 7; page++ {
+				wg.Add(1)
+				go func(urlTemplate string, page int) {
+					defer wg.Done()
+					url := fmt.Sprintf(urlTemplate, page)
+					parseURL(url, productChan)
+				}(urlTemplate, page)
+			}
 		}
 	}
 
@@ -125,10 +115,14 @@ func main() {
 		close(productChan)
 	}()
 
-
 	var products []ProductData
+	productMap := make(map[int]bool)
+
 	for product := range productChan {
-		products = append(products, product)
+		if !productMap[product.ProductID] {
+			productMap[product.ProductID] = true
+			products = append(products, product)
+		}
 	}
 
 	jsonData, err := json.MarshalIndent(products, "", "  ")
@@ -146,8 +140,55 @@ func main() {
 	fmt.Println("Data saved to products.json")
 }
 
-func parseURL(url string, productChan chan<- ProductData) {
+func getCategoryURLs(url string) ([]string, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
 
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var data struct {
+		Items []Category `json:"items"`
+	}
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		return nil, err
+	}
+
+	var urls []string
+	for _, item := range data.Items {
+		urls = append(urls, extractURLs(item)...)
+	}
+
+	return urls, nil
+}
+
+func extractURLs(category Category) []string {
+	var urls []string
+	if category.URL != "" {
+		// Заменяем "catalog" на "section"
+		category.URL = strings.Replace(category.URL, "catalog", "section", 1)
+		// Пропускаем URL, содержащие "#gift"
+		if !strings.Contains(category.URL, "#gift") && !strings.Contains(category.URL, "/section/kids_all"){
+			urls = append(urls, generateCatalogURL(category.URL))
+		}
+	}
+	for _, item := range category.Items {
+		urls = append(urls, extractURLs(item)...)
+	}
+	return urls
+}
+
+func generateCatalogURL(catalogCode string) string {
+	return fmt.Sprintf("https://lime-shop.com/api%s?page_size=30", catalogCode)
+}
+
+func parseURL(url string, productChan chan<- ProductData) {
 	resp, err := http.Get(url)
 	if err != nil {
 		fmt.Printf("Error making HTTP request for URL %s: %v\n", url, err)
@@ -164,7 +205,7 @@ func parseURL(url string, productChan chan<- ProductData) {
 	var data struct {
 		Items []struct {
 			Cells []struct {
-				Entity Product `json:"entity"`
+				Entity Item `json:"entity"`
 			} `json:"cells"`
 		} `json:"items"`
 	}
@@ -177,30 +218,30 @@ func parseURL(url string, productChan chan<- ProductData) {
 	for _, item := range data.Items {
 		for _, cell := range item.Cells {
 			for _, model := range cell.Entity.Models {
-				for _, sku := range model.Skus {
-					productURL := generateProductURL(cell.Entity.Code, model.Code)
+				productURL := generateProductURL(cell.Entity.Code, model.Code)
 
-
-					productData := ProductData{
-						Name:        cell.Entity.Name,
-						Price:       sku.Price,
-						OldPrice:    sku.OldPrice,
-						Size:        []Size{sku.Size},
-						Count: 		 []Stock{sku.Stock},
-						Description: cell.Entity.Description,
-						Color:       []Color{model.Color},
-						Article:     cell.Entity.Article,
-						ProductURL:  []string{productURL},
-						Category:    model.Category,
-						ProductID:   cell.Entity.ProductID, 
-						Composition: cell.Entity.Composition,
-						Care:        cell.Entity.Care,
-						Medias:      getMediaURLs(model.Medias),
-					}
-
-					productChan <- productData
-					
+				productData := ProductData{
+					Name:        cell.Entity.Name,
+					Description: cell.Entity.Description,
+					Article:     cell.Entity.Article,
+					ProductURL:  []string{productURL},
+					Category:    model.Category,
+					ProductID:   cell.Entity.ProductID,
+					Composition: cell.Entity.Composition,
+					Care:        cell.Entity.Care,
+					Medias:      getMediaURLs(model.Medias),
+					Color:       []Color{model.Color},
 				}
+
+				// Добавляем информацию о размерах и ценах
+				for _, sku := range model.Skus {
+					productData.Size = append(productData.Size, sku.Size)
+					productData.Count = append(productData.Count, sku.Stock)
+					productData.Price = sku.Price
+					productData.OldPrice = sku.OldPrice
+				}
+
+				productChan <- productData
 			}
 		}
 	}
@@ -209,7 +250,7 @@ func parseURL(url string, productChan chan<- ProductData) {
 func getMediaURLs(medias []Media) []string {
 	var urls []string
 	for _, media := range medias {
-		urls = append(urls, media.URLs)
+		urls = append(urls, media.URL)
 	}
 	return urls
 }
